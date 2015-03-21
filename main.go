2f60a0bc00a980820de8376113f1008529412c36
Matt Jaffee <jaffee@umbel.com>
2015-03-20 23:41:13 -0500

    add some template stuff and another page

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Post struct {
+	Title string
+}
+
 func serveRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Letsssssss goooooooo! %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "<html><body>Letsssssss goooooooo! %s\n</body></html>", r.URL.Path[1:])
+}
+
+func serveDay(w http.ResponseWriter, r *http.Request) {
+	date := r.URL.Path[len("/day/"):]
+	post := &Post{Title: date}
+	t, _ := template.ParseFiles("day.html")
+	t.Execute(w, post)
 }
 
 func main() {
 	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/day/", serveDay)
 	http.ListenAndServe(":8080", nil)
 }
